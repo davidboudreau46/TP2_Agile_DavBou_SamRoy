f08c3ec4faf7fdbf6c50a8245886e5dc95e98fd5
David_Boudreau <david.boudreau46@gmail.com>
2016-12-06 17:13:07 -0500

    config travis

--- start-web.sh
+++ start-web.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 docker build -t web_image -f ./docker/web/Dockerfile  .
-docker run -it --rm -p5000:5000 --link postgresdb:postgres -v $HOME/temp/publish:/root/publish -v $HOME/.nuget/packages:/root/.nuget/packages -v $PWD/NameGenerator:/NameGenerator -e WEBAPP_DATABASE_CONNECTIONSTRING=$WEBAPP_DATABASE_CONNECTIONSTRING web_image
+docker run -it --rm --link SamDavPGDB -e WEBAPP_DATABASE_CONNECTIONSTRING=$WEBAPP_DATABASE_CONNECTIONSTRING web_image
 
 
