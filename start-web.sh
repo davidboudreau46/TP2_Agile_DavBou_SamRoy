7d499fa7b9b12be87598cd4ae1087cddeb418c06
Samuel Roy <samuel.roy07@gmail.com>
2016-12-08 16:02:38 -0500

    test

--- start-web.sh
+++ start-web.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker build -t samroydavbou/web_image:latest -f ./docker/web/Dockerfile  .
+docker start SamDavPGDB
 docker run -it --rm --name SamDavWebApp --link SamDavPGDB -e WEBAPP_DATABASE_CONNECTIONSTRING=$WEBAPP_DATABASE_CONNECTIONSTRING samroydavbou/web_image
 
 
