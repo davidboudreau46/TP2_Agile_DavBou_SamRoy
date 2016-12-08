8bd42b585f7b95a21d81072af872125dd8e56e88
Samuel Roy <samuel.roy07@gmail.com>
2016-12-08 11:42:12 -0500

    without digital ocean

--- start-db.sh
+++ start-db.sh
@@ -3,5 +3,5 @@ set -v
 docker stop SamDavPGDB
 docker rm SamDavPGDB
 docker build -t samroydavbou/db_image:latest -f ./docker/db/Dockerfile  .
-docker run --name SamDavPGDB -p 5432:5432 -e POSTGRES_PASSWORD=$ROOT_POSTGRES_PASSWORD  -d samroydavbou/db_image:latest
+docker run --name SamDavPGDB -e POSTGRES_PASSWORD=$ROOT_POSTGRES_PASSWORD  -d samroydavbou/db_image:latest
 docker ps
